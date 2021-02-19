e2b3675342ef104309754735e4db67058a12cab9
Ernan <44041+ernan@users.noreply.github.com>
2021-02-18 22:02:51 -0500

    main add an erro page add README.md add a test case

--- src/main/java/com/example/controller/IndividualController.java
+++ src/main/java/com/example/controller/IndividualController.java
@@ -9,7 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndividualController {
